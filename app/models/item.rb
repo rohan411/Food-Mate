fe5dd45158126d2c96a1a6efed1db30092ae39dd
Pushp Raj Saurabh <pushprajsaurabh@gmail.com>
2015-05-29 22:40:54 +0530

    Added items controller and get_items action

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,14 @@ class Item < ActiveRecord::Base
  def get_url
   "#{Rails.root}/public" + self.logo.url(:medium)
  end
- 
+
+ def to_item_hash
+   return {
+    :item_name => self.name,
+    :item_image_url => self.get_url,
+    :collection_id => self.collection_id
+   }
+ end
+
 end
 
