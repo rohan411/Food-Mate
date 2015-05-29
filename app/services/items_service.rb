2188b3dd9c930070950f385c11b5a866317c953d
zunaid <mailtozunaid@gmail.com>
2015-05-30 05:04:22 +0530

    big fix

--- app/services/items_service.rb
+++ app/services/items_service.rb
@@ -21,11 +21,12 @@ class ItemsService
       raise StandardError.new('Item not found against the item_id')
     end
     choice = UserChoice.find_or_create_by(:user_id => user_id)
-    chosen_item_ids = choice.item_ids rescue nil
-    choice.item_ids = chosen_item_ids.to_s + "," + params[:item_id].to_s
-    chosen_tags = choice.tags rescue nil
-    tags_unsorted = chosen_tags.to_s + "," + item.tags.pluck(:name).map {|str| "#{str}"}.join(',')
-    choice.tags = tags_unsorted.split(",").sort.map {|str| "#{str}"}.join(',')
+    chosen_item_ids = choice.item_ids + ',' rescue ''
+    item_ids_unsorted = chosen_item_ids.to_s + params[:item_id].to_s
+    choice.item_ids = item_ids_unsorted.split(",").sort.map { |str| "#{str}" }.join(',')
+    chosen_tags = choice.tags + ',' rescue ''
+    tags_unsorted = chosen_tags.to_s + item.tags.pluck(:name).map {|str| "#{str}"}.join(',')
+    choice.tags = tags_unsorted.split(",").sort.map { |str| "#{str}" }.join(',')
     choice.save
   end
 
