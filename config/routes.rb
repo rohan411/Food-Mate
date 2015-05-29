faa19f629a23a14f66f97bd59e899c0d1ee3bf20
zunaid <mailtozunaid@gmail.com>
2015-05-29 19:22:49 +0530

    rm mc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#create", :as => "log_in"
-  get "sign_up" => "users#create", :as => "sign_up"
+  post "log_out" => "sessions#destroy", :as => "log_out"
+  post "log_in" => "sessions#create", :as => "log_in"
+  post "sign_up" => "users#create", :as => "sign_up"
   # root :to => "users#new"
   resources :users
   resources :sessions
